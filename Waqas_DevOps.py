5498b614bea3fe53b802c219658336b444fd2c64
wickijutt <wickijutt@gmail.com>
2024-06-25 14:37:02 +0500

    waqas20240625

---
+++ Waqas_DevOps.py
@@ -0,0 +1,16 @@
+# Get the username and password from the user
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Print out the username and password
+print("Username:", username)
+print("Password:", password)
+
+import getpass
+
+# Get the username and password from the user
+username = input("Enter your username: ")
+password = getpass.getpass("Enter your password: ")
+
+# Print out the username (but not the password for security reasons)
+print("Username:", username)
