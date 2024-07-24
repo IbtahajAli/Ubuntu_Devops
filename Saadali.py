411c4f091b8ccb77465afcacb9159f5a90bb5049
muhammadwaqas964 <124439937+muhammadwaqas964@users.noreply.github.com>
2024-07-24 17:30:11 +0500

    Create Saadali.py

---
+++ Saadali.py
@@ -0,0 +1,18 @@
+# Get the username and password from the user
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Print out the username and password
+print("Username:", username)
+print("Password:", password) 
+
+import getpass
+
+# Get the username and password from the user
+username = input("Enter your username: ")
+password = getpass.getpass("Enter your password: ")
+
+# Print out the username (but not the password for security reasons)
+print("Username:", username)
+print("name:")
+print("hello world,saad ") 
